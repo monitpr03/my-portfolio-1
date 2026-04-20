5d53826a89b9c03e2164f07f3067c34637cc9123
monitpr03 <monitpr03@gmail.com>
2026-04-20 09:07:36 +0530

    Fixed some issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+<<<<<<< HEAD
+=======
+    base: './',
+>>>>>>> 634f385 (Fixed some issues)
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
